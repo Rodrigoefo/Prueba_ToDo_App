79d7a2dfccadb62db9cab0f7801eb1dd73575674
Rodrigo <fuentesosorio@fuentesosorio.com>
2019-07-11 13:06:13 -0400

    ToDo Project

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,66 @@
+class TodosController < ApplicationController
+
+  def index
+    @todos=Todo.all
+  end
+
+  def new
+    @todo=Todo.new
+  end
+
+  def create
+    @todo=Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+
+  end
+
+  def show
+    @todo=Todo.find(params[:id])
+  end
+
+  def edit
+    @todo=Todo.find(params[:id])
+  end
+
+  def update
+    @todo=Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo=Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  def complete
+  
+    @todo=Todo.find(params[:id])
+      if @todo.completed == true
+          @todo.completed=false
+          @todo.save
+          redirect_to todos_path
+      else
+        @todo.completed=true
+        @todo.save
+        redirect_to todos_path
+      end
+
+  end
+
+  def list
+    @todos=Todo.all
+
+  end
+
+
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+
+
+end
